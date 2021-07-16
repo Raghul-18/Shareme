47fd40d048455a66c6e95f0eb4b221cfc5f7d61c
Raghul-18 <87120429+Raghul-18@users.noreply.github.com>
2021-07-16 22:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ const onFileUploadSuccess = (res) => {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-    },
+  },
     body: JSON.stringify(formData),
   })
     .then((res) => res.json())
